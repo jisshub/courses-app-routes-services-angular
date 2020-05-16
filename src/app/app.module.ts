6b9d0866109bab9ebbeb4c321eebdd76f6defb3f
jisshub <jissmon476@gmail.com>
2020-05-16 11:44:42 +0530

    courses

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CoursesEditComponent } from './courses/courses-edit/courses-edit.component';
+import { TrainersComponent } from './trainers/trainers.component';
+import { TrainersEditComponent } from './trainers/trainers-edit/trainers-edit.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CoursesComponent,
+    CoursesEditComponent,
+    TrainersComponent,
+    TrainersEditComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/trainers/trainers-edit/trainers-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trainers-edit',
+  templateUrl: './trainers-edit.component.html',
+  styleUrls: ['./trainers-edit.component.css']
+})
+export class TrainersEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
